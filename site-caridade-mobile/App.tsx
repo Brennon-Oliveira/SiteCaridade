ad7e092b45b521d7ebe5a9b48b4c7ff02ba2a8ef
Brennon Oliveira <brennonoliveira20014@gmail.com>
2021-03-20 00:29:45 -0300

    Inicio da branch mobileApp

--- site-caridade-mobile/App.tsx
+++ site-caridade-mobile/App.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
-import { StatusBar } from 'react-native';
+import { ScrollView, StatusBar, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default class App extends React.Component {
   render() {
     return (
-      <WebView
-        originWhitelist={['*']}
-        source={{ uri: 'https://localhost:8050' }}
-      >
-        <StatusBar hidden />
-      </WebView>
+      <ScrollView>
+        <Text>Ola</Text>
+      </ScrollView>
     );
   }
 }
